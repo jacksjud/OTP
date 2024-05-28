253b20f805a089845b4cf7f42580f54bcec2f289
Judah Jackson <jacksjud@flip2.engr.oregonstate.edu>
2024-05-28 14:53:01 -0700

    file creation

---
+++ dec_client.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+
+
